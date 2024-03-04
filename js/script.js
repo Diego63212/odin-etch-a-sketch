316d2516808491602fe06f7814563bb7af2701e2
Diego63212 <doom63212@gmail.com>
2024-03-03 22:40:20 -0300

    General cleanup

--- js/script.js
+++ js/script.js
@@ -2,24 +2,17 @@ const btnChange = document.querySelector('#btn-change')
 const pixelContainer = document.querySelector('#pixel-container');
 const containerSize = pixelContainer.offsetHeight;
 let pixelCount = 16;
-/* let pixelSize = containerSize / pixelCount - 1 + 'px'; */
 
 pixelContainer.addEventListener('mouseover', pixelHighlight);
-/* pixelContainer.addEventListener('mouseout', pixelFade); */
-btnChange.addEventListener('click', (event) => {
-    let userPixelCount = prompt('How many pixels per side?');
+btnChange.addEventListener('click', () => {
+    // Set minimum to 1 and maximum of 100 to conserve performance
+    let userPixelCount = Math.max(Math.min(prompt('How many pixels per side?'), 100), 1);
     drawPixels(userPixelCount);
 })
 
-function pixelHighlight(event) {
-    if (event.target.classList.contains('pixel')) {
-        event.target.style.backgroundColor = 'yellow';
-    }
-}
-
-function pixelFade(event) {
-    if (event.target.classList.contains('pixel')) {
-        event.target.style.backgroundColor = '';
+function pixelHighlight(e) {
+    if (e.target.classList.contains('pixel')) {
+        e.target.style.backgroundColor = 'yellow';
     }
 }
 
@@ -27,17 +20,15 @@ function pixelFade(event) {
 let fragment = document.createDocumentFragment();
 
 function drawPixels(amount) {
-    let pixelSize = containerSize / amount - 1 + 'px';
-    console.log(pixelSize);
+    let pixelSize = containerSize / amount + 'px';
 
+    pixelContainer.replaceChildren('')
     for (let i = 0; i < amount * amount; i++) {
-        let pixelDiv = document.createElement('div');
-    /*     pixelDiv.style.backgroundColor = 'yellow'; */
+        const pixelDiv = document.createElement('div');
         pixelDiv.style.height = pixelSize;
         pixelDiv.style.width = pixelSize;
         pixelDiv.classList.add('pixel')
         fragment.appendChild(pixelDiv);
-    /*     pixelContainer.appendChild(pixelDiv); */
     }
     pixelContainer.replaceChildren(fragment);
 }
