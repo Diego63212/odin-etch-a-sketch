1c229e7997eed635a1ec889e30d001c6fb4cfb2b
Diego63212 <doom63212@gmail.com>
2024-09-30 17:40:31 -0300

    Minor JS changes
    * Implement regular expression unpacking.
    * Increase maximum pixel amount to 200.
    * Reformat function code.
    * Move code around.

--- js/script.js
+++ js/script.js
@@ -1,49 +1,11 @@
 const btnChange = document.querySelector('#btn-change');
 const btnReset = document.querySelector('#btn-reset');
 const pixelContainer = document.querySelector('#pixel-container');
+const fragment = document.createDocumentFragment(); // Use document fragment for extra performance
 let pixelCount = 16;
 
-// Listen to bubbling mouseover event for better performance
-pixelContainer.addEventListener('mouseover', pixelHighlight);
-
-btnChange.addEventListener('click', () => {
-    // Set minimum to 1 and maximum of 100 to conserve performance
-    pixelCount = Math.max(Math.min(prompt('How many pixels per side? (Maximum is 100)', '16'), 100), 1);
-    drawPixels(pixelCount);
-})
-
-btnReset.addEventListener('click', () => {
-    drawPixels(pixelCount);
-})
-
-function randomRGB() {
-    return Math.floor(Math.random() * 255);
-}
-
-function pixelHighlight(e) {
-    // Gives a random color only to unmodified pixel class elements
-    if (e.target.classList.contains('pixel') && e.target.style.backgroundColor == '') {
-        e.target.style.backgroundColor = 'rgb('+randomRGB()+','+randomRGB()+', '+randomRGB()+')';
-    } else if (e.target.classList.contains('pixel')) {
-        darkenColor(e);
-    }
-}
-// Divides individual colors to darken them over reiterated interactions
-function darkenColor(e) {
-    // Store regular expression groups into an array for easy data access
-    let lastColor = Array.from(/rgb\((\d*), (\d*), (\d*)\)/.exec(e.target.style.backgroundColor));
-    let colorRed = Math.floor(lastColor[1] / 1.5);
-    let colorGreen = Math.floor(lastColor[2] / 1.5);
-    let colorBlue = Math.floor(lastColor[3] / 1.5);
-    e.target.style.backgroundColor = `rgb(${colorRed}, ${colorGreen}, ${colorBlue})`;
-}
-
-// Use document fragment for extra performance
-let fragment = document.createDocumentFragment();
-
 function drawPixels(amount) {
-    let pixelSize = 100 / amount + '%';
-
+    const pixelSize = 100 / amount + '%';
     for (let i = 0; i < amount * amount; i++) {
         const pixelDiv = document.createElement('div');
         pixelDiv.style.height = pixelSize;
@@ -54,4 +16,40 @@ function drawPixels(amount) {
     pixelContainer.replaceChildren(fragment);
 }
 
+function randomRGB() {
+    return Math.floor(Math.random() * 255);
+}
+
+function pixelHighlight(e) {
+    const pixel = e.target;
+    // Gives a random color only to unmodified pixel class elements
+    if (!pixel.style.backgroundColor) {
+        pixel.style.backgroundColor = 'rgb('+randomRGB()+','+randomRGB()+','+randomRGB()+')';
+    } else {
+        darkenColor(pixel);
+    }
+}
+// Divides individual colors to darken them over reiterated interactions
+function darkenColor(pixel) {
+    // Unpack regular expression groups into variables for easy data access
+    let [, colorRed, colorGreen, colorBlue] = /(\d+), (\d+), (\d+)/.exec(pixel.style.backgroundColor);
+    colorRed = Math.floor(colorRed / 1.5);
+    colorGreen = Math.floor(colorGreen / 1.5);
+    colorBlue = Math.floor(colorBlue / 1.5);
+    pixel.style.backgroundColor = `rgb(${colorRed}, ${colorGreen}, ${colorBlue})`;
+}
+// Listen to bubbling mouseover event for better performance
+pixelContainer.addEventListener('mouseover', pixelHighlight);
+
+btnChange.addEventListener('click', () => {
+    // Set minimum to 1 and maximum of 200 to conserve performance
+    pixelCount = Math.max(Math.min(prompt('How many pixels per side? (Maximum is 200)', '16'), 200), 1);
+    drawPixels(pixelCount);
+});
+
+btnReset.addEventListener('click', () => {
+    drawPixels(pixelCount);
+});
+
+// Initialize with default value
 drawPixels(pixelCount);
